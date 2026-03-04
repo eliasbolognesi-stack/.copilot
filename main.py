e5dbeef314921e7df9e96c3d96401eee5111b86d
Elias <elias.bolognesi@escola.pr.gov.br>
2026-03-04 12:10:01 -0300

    p

---
+++ main.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+data = [np.random.normal(0, std, 10) for std in range(1, 4)]
+
+plt.boxplot(data, vert=True, patch_artist=True)
+plt.xlabel('Grupo')
+plt.ylabel('Valor')
+plt.title('Box Plot')
+plt.show() 
+
+data = np.random.randn(1000)
+
+plt.hist(data, bins=30, alpha=0.75, color='blue')
+plt.xlabel('Valor')
+plt.ylabel('Frequência')
+plt.title('Histograma')
+plt.show()
